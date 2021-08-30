87040e0545ca64b20f37edc132103cb9b368c797
pohjanjoki <77720618+pohjanjoki@users.noreply.github.com>
2021-08-30 10:18:17 +0300

    na

--- Henkilön viimeiset tiedot.sql
+++
@@ -1,4 +0,0 @@
-DROP VIEW "main"."henkilon_viimeiset_tiedot";
-CREATE VIEW henkilon_viimeiset_tiedot AS
-SELECT henkilon_viimeisin_mittaus.henkilo_id, etunimi, sukunimi, sukupuoli, pituus, paino, ika
-FROM henkilon_viimeisin_mittaus INNER JOIN henkilo_mittaus_ika_v2 ON henkilon_viimeisin_mittaus.viimeisin_mittaus_id = henkilo_mittaus_ika_v2.mittaus_id
